ee811ccbbb74cd084f10b66185c0ea7441a8e722
lindomarc <lindomarc@outlook.com>
2021-07-11 12:36:12 -0300

    feat: configured  vue-routes

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,25 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
+import Vue from "vue";
+
 require('./bootstrap');
 
 window.Vue = require('vue').default;
+
+/**
+ * Routes imports and assigning
+ */
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+import routes from './routes';
+
+let router = new VueRouter({
+    mode: 'history',
+    routes
+});
+// Routes End
+
 
 /**
  * The following block of code may be used to automatically register your
@@ -19,7 +35,7 @@ window.Vue = require('vue').default;
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -29,4 +45,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    router
 });

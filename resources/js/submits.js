c9806d708fe36e02284c9af12332fb4667ef79c1
lindomarc <lindomarc@outlook.com>
2021-07-16 13:00:51 -0300

    feat: message toast

--- resources/js/submits.js
+++ resources/js/submits.js
@@ -1,21 +1,25 @@
- 
+import toast from "./toast";
+
 export default {
+    mixins: [toast],
     methods: {
         listLatest(model, progress = true) {
             progress?this.$Progress.start():'';
             axios.get(`api/${model}`).then(({data}) => {
                 this.items = data.data;
             })
+            
             progress?this.$Progress.finish():'';
         },
         create(model) {
             this.$Progress.start()
-            this.form.post(`api/${model}`).then((response) => {
-                // console.log('Everything is awesome.');
+            this.form.post(`api/${model}`).then((response) => {                
                 this.$refs['createModal'].hide()
                 this.listLatest(model,false);
+                this.toast('Ele já foi listado','Item criado com sucesso', 'success');
             }).catch((error) => {
                 //console.warn('Not good man :(');
+                this.toast('Reveja as informações no formulário','Item não foi salvo', 'danger');
             });
             this.$Progress.finish()
 

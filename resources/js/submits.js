b7b8af22b2263accebb1b431c46a61a80597fcc8
lindomarc <lindomarc@outlook.com>
2021-07-16 11:41:45 -0300

    refactor: remove functions only tests

--- resources/js/submits.js
+++ resources/js/submits.js
@@ -23,34 +23,6 @@ export default {
                 let parse_mail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
                 return parse_mail.test(email);
             }
-        },
-        start () {
-            this.$Progress.start()
-        },
-        set (num) {
-            this.$Progress.set(num)
-        },
-        increase (num) {
-            this.$Progress.increase(num)
-        },
-        decrease (num) {
-            this.$Progress.decrease(num)
-        },
-        finish () {
-            this.$Progress.finish()
-        },
-        fail () {
-            this.$Progress.fail()
-        },
-        test(){
-            this.$Progress.start()
-
-            this.$http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json?apikey=7waqfqbprs7pajbz28mqf6vz')
-                .then((response) => {
-                    this.$Progress.finish()
-                }, (response) => {
-                    this.$Progress.fail()
-                })
         }
     },
 

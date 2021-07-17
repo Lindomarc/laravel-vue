a3cca5565b34dbcc8b2ec21774e8cc1f3ca211ed
lindomarc <lindomarc@outlook.com>
2021-07-17 01:57:44 -0300

    fix: install sectum'

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class UserController extends Controller
 {
@@ -19,13 +19,13 @@ class UserController extends Controller
     {
         return User::latest()->paginate(10);
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
+	
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param Request $request
+	 * @return array
+	 */
     public function store(Request $request)
     {
 	    $request->validate([
@@ -34,8 +34,8 @@ class UserController extends Controller
 		    'type'=>'required',
 		    'password' => 'required|min:8|confirmed'
 	    ]);
-	    
-    	$data = [
+	
+	    $data = [
 		    'name' => $request['name'],
 		    'email' => $request['email'],
 		    'type' => $request['type'],
@@ -43,41 +43,48 @@ class UserController extends Controller
 		    'photo' => $request['photo'],
 		    'password' => Hash::make($request['password'])
 	    ];
-	    User::create($data);
-	    return [
-		    'message'=> __('User created with successfully.'),
-		    'variant' => 'success'
+	    $user = User::create($data);
+	
+	    $token = $user->createToken($request->email)->plainTextToken;
+	
+	    $response = [
+		    'message' => __('User created with successfully.'),
+		    'variant' => 'success',
+		    'user' => $user,
+		    'token' => $token
 	    ];
+	
+	    return response($response);
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return array
-     */
-    public function update(User $user, Request $request)
-    {
-	    $request->validate([
-		    'name'=> 'required|min:3',
-		    'email' => 'email:rfc,dns|unique:users,email,'.$user['id'],
-		    'type'=>'required',
-		    'password' => 'sometimes|required|min:8|confirmed'
-	    ]);
-	    
-    	$user->fill($request->all());
+	
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param int $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+	
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param User $user
+	 * @param Request $request
+	 * @return array
+	 */
+	public function update(User $user, Request $request)
+	{
+		$request->validate([
+			'name' => 'required|min:3',
+			'email' => 'email:rfc,dns|unique:users,email,' . $user['id'],
+			'type' => 'required',
+			'password' => 'sometimes|required|min:8|confirmed'
+		]);
+		
+		$user->fill($request->all());
 	    $user->save();
 	
 	    return [
@@ -92,13 +99,39 @@ class UserController extends Controller
      * @param  int  $id
      * @return array
      */
-    public function destroy($id)
-    {
-    	$user = User::findOrFail($id);
-    	$user->delete();
-    	return [
-    		'message'=> __('User removed successfully.'),
-		    'variant' => 'success'		    
-	    ];
-    }
+	public function destroy($id)
+	{
+		$user = User::findOrFail($id);
+		$user->delete();
+		return [
+			'message' => __('User removed successfully.'),
+			'variant' => 'success'
+		];
+	}
+	
+	/**
+	 * @throws ValidationException
+	 */
+	public function login(Request $request)
+	{
+		$request->validate([
+			'email' => 'required|email',
+			'password' => 'required',
+		]);
+		$user = User::where('email', $request->email)->first();
+		
+		if (!$user || !Hash::check($request->password, $user->password)) {
+			throw ValidationException::withMessages([
+				'email' => ['The provided credentials are incorrect.'],
+			]);
+		}
+		
+		$token = $user->createToken($request->email)->plainTextToken;
+		
+		$response = [
+			'user' => $user,
+			'token' => $token
+		];
+		return response($response, 201);
+	}
 }
--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,12 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use function Symfony\Component\String\u;
+
 class LoginController extends Controller
 {
     /*
@@ -37,4 +43,5 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+    
 }
